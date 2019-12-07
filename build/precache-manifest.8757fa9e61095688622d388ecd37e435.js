d92c679e4e5658e5779b8435d4ac3342a953a960
Artsiom Pliashkun <asperatus@inbox.ru>
2019-12-07 23:31:26 +0300

    Add files via upload

---
+++ build/precache-manifest.8757fa9e61095688622d388ecd37e435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498ac9930af80565058265c993426a07",
+    "url": "/app-name/index.html"
+  },
+  {
+    "revision": "a4f3c0e9bf31a2c8a3b1",
+    "url": "/app-name/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "ab650991a3f129e74a24",
+    "url": "/app-name/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a4f3c0e9bf31a2c8a3b1",
+    "url": "/app-name/static/js/2.9dd7a8e7.chunk.js"
+  },
+  {
+    "revision": "ab650991a3f129e74a24",
+    "url": "/app-name/static/js/main.87970621.chunk.js"
+  },
+  {
+    "revision": "0b03167f7267d3737875",
+    "url": "/app-name/static/js/runtime-main.d64cc589.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/app-name/static/media/persik.4e1ec840.png"
+  }
+]);
