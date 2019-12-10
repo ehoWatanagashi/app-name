000bcda387e2fecda481779fff1344de87b7e209
ehoWatanagashi <asperatus@inbox.ru>
2019-12-10 15:05:52 +0300

    Updates

---
+++ precache-manifest.fc1f48b1f87b4e9d930331f20b64c35d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3f8b5ecdf4014581d91873c9d04504",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70e0296659e758c6687b",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "0e9206c59b874d08a86b",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "70e0296659e758c6687b",
+    "url": "./static/js/2.17a97f21.chunk.js"
+  },
+  {
+    "revision": "0e9206c59b874d08a86b",
+    "url": "./static/js/main.aa7b166b.chunk.js"
+  },
+  {
+    "revision": "5f1561a8ea7870f7bb70",
+    "url": "./static/js/runtime-main.826def18.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
